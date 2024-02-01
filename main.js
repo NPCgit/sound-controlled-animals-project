751fb3d4fa2b40df0bf967814fb26d546e5d51e4
NPCgit <119781990+NPCgit@users.noreply.github.com>
2024-02-01 19:20:26 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// cant record lol my microphone cant hear from phone and cant upload audio files to teachable not everyone is poor XD
+// cant record lol my microphone cant hear from phone and cant upload audio files to teachable not everyone has one that picks up every noise in the world
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier("'https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json", modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
