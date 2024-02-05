054931559cf424fc183d418f4eb460e25c15d005
NPCgit <119781990+NPCgit@users.noreply.github.com>
2024-02-05 17:18:18 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
-// cant record lol my microphone cant hear from phone and cant upload audio files to teachable not everyone has one that picks up every noise in the world
-
 function startClassification()
 {
     navigator.mediaDevices.getUserMedia({ audio: true});
-    classifier = ml5.soundClassifier("'https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json", modelReady);
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/u02fOYRBx/model.json", modelReady);
 }
 
 function modelReady()
 {
     classifier.classify(gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = "I can hear - " + results[0].label;
+        document.getElementById("result_confidence").innerHTML = "Accuracy - " + (results[0].confidence * 100).toFixed(2) + "%"
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color = "rgb(" + random_number_r+","+random_number_g+","+random_number_b+")";
+
+        img = document.getElementById("audio");
+        if(results[0].label == "Background Noise")
+        {
+            img.src = "listen.gif";
+        } else if(results[0].label == "Barking")
+        {
+            img.src = "bark.gif";
+        } else{
+            img.src = "meow.gif";
+        }
+        
+    }
+
 }
